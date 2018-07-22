9de928d7777b26bd98d64a1368b054bbdfa3ba82
isabellebidou <isa.bidou@gmail.com>
2018-07-22 13:06:45 +0000

    added mysql, layout , asanas and muscles

---
+++ behaviour.js
@@ -0,0 +1,37 @@
+var mobileMenu = false
+var mobileMenuDiv = false;
+
+function initialize() {
+	console.log("initialize!");
+	mobileMenuDiv = document.getElementById("mobilemenu");
+	mobileMenuDiv.style.display = "none";
+	var x = "Total Width: " + screen.width + "px";
+	console.log(x);
+}
+function toggleMenu() {
+
+	mobileMenu = !mobileMenu;
+	if (mobileMenu) {
+		document.getElementById("mobilemenu").style.display = "inline-block";
+
+	} else {
+		document.getElementById("mobilemenu").style.display = "none";
+	}
+}
+
+function closeMenu() {
+	console.log("closemenu!");
+	if (mobileMenu) {
+		document.getElementById("mobilemenu").style.display = "inline-block";
+
+	}
+
+}
+
+module.exports = {
+  initialize : initialize(),
+  toggleMenu: toggleMenu(),
+  closeMenu: closeMenu()
+  
+  
+}
